23beb1f2b99ade7867635ac992d1f9c6ed140390
RupiniRudrappan <46262915+rupinirudrappan@users.noreply.github.com>
2019-09-10 08:33:50 +0530

    Update bitwise or.py

--- bitwise or.py
+++ bitwise or.py
@@ -1,7 +1,15 @@
 array=[]
 result=0
-size=int(input())
-array=list(map(int,input().split()))
-for i in array:
-  result=result|i
-print(result)
+try:
+  size=int(input())
+  array=list(map(int,input().split()))
+  if len(array)==size:
+    for i in array:
+      result=result|i
+    print(result)
+  else:
+    raise IndexError
+except IndexError:
+  pass
+except ValueError:
+  pass
